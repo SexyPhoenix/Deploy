e98f28171cf9b8759862559625ac19473f9d8e52
SexyPhoenix <zhangpengsheng@focuschina.com>
2019-12-04 11:11:27 +0800

    add env

---
+++ Envoy.blade.php
@@ -0,0 +1,15 @@
+@servers(['app-1' => 'root@192.168.10.12', 'app-2' => 'root@192.168.10.18'])
+
+@task('deploy', ['on' => ['app-1', 'app-2']])
+	cd /var/www
+	ls -la
+@endtask
+
+@task('init', ['on' => ['app-1', 'app-2']])
+	mkdir -p /var/www/
+	cd /var/www/
+	git clone git@github.com:SexyPhoenix/Deploy.git
+	cd Deploy
+        composer install --no-dev
+        chmod -R 0777 storage
+@endtask
